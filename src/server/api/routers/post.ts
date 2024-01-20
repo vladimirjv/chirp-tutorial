aa356db75e2468f2b887d544ed7ab83e008db42a
Vladimir JV <ivladimirjuarez@outlook.com>
2024-01-19 18:37:24 -0600

    add post users info and data

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,6 +1,16 @@
+import { User, clerkClient } from "@clerk/nextjs/server";
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+const filerUserForClient = (user: User) => {
+  return {
+    id: user.id,
+    name: user.username ?? user.firstName,
+    profileImageURL: user.profileImageUrl
+  }
+}
 
 export const postRouter = createTRPCRouter({
   hello: publicProcedure
@@ -10,9 +20,36 @@ export const postRouter = createTRPCRouter({
         greeting: `Hello ${input.text}`,
       };
     }),
-    getAll: publicProcedure.query(({ ctx }) => {
-      return ctx.db.post.findMany();
-    }),
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.db.post.findMany({
+      take: 100,
+      // where: {
+      //   authorId: ctx.user?.id,
+      // }
+    });
+
+    const users = (await clerkClient.users.getUserList({
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+      userId: posts.map((post) => post.authorId),
+      limit: 100,
+    })).map(filerUserForClient);
+
+    // console.log(users);
+
+    return posts.map((post) => {
+      const user = users.find((user) => user.id === post.authorId);
+      if (!user) {
+        throw new TRPCError({
+          code: 'INTERNAL_SERVER_ERROR',
+          message: "User not found for post"
+        });
+      }
+      return {
+        post,
+        user
+      }
+    });
+  }),
 
   // create: publicProcedure
   //   .input(z.object({ name: z.string().min(1) }))
